cf05027d02b7dd147b33648de72d08fbafdeff1e
RtaMoreira <rita.moreira@students.hevs.ch>
2019-11-20 00:03:50 +0200

    .

--- src/main/java/fi/haagahelia/blogmanagment/WebSecurityConfig.java
+++ src/main/java/fi/haagahelia/blogmanagment/WebSecurityConfig.java
@@ -1,17 +1,13 @@
 package fi.haagahelia.blogmanagment;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.firewall.HttpFirewall;
-import org.springframework.security.web.firewall.StrictHttpFirewall;
 
 import fi.haagahelia.blogmanagment.web.UserDetailServiceImpl;
 
@@ -23,7 +19,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private UserDetailServiceImpl userDetailsService;	
 	
-    protected void configure(HttpSecurity http, WebSecurity web) throws Exception {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
         http
         .authorizeRequests().antMatchers("/","/css/**","/articles/**","/register","/article/**","/image/**","/h2_console/**","/console/**").permitAll() // Enable css when logged out
         .and()
@@ -39,7 +36,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         
         http.csrf().disable();
         http.headers().frameOptions().disable();
-        web.httpFirewall(allowUrlEncodedSlashHttpFirewall());
     }
     
     @Autowired
@@ -47,15 +43,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
     }
 
-    
-    //to allow "//" into spring firewall (because of the encoded image in base64)
-    @Bean
-    public HttpFirewall allowUrlEncodedSlashHttpFirewall() {
-        StrictHttpFirewall firewall = new StrictHttpFirewall();
-        firewall.setAllowUrlEncodedSlash(true);
-        firewall.setAllowUrlEncodedDoubleSlash(true);
-        return firewall;
-    }
 
     
 }
